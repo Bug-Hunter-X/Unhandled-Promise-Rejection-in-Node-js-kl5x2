5625cd44a76173cfd8caea1d09fb8611d2d947c8
BugHunter-X <askzenai@gmail.com>
2025-01-25 18:00:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//Unhandled promise rejection in case of error handling
